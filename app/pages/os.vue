731bac8bd10d8602dc1567af7fa9def60baf9123
Hencter <hencter@foxmail.com>
2025-02-21 13:57:16 +0800

    Initial commit

---
+++ app/pages/os.vue
@@ -0,0 +1,33 @@
+<template>
+	<LayoutTile
+		title="OS Information"
+		description="Read information about the operating system using the OS Information plugin."
+	>
+		<UAccordion :items="items" type="multiple" />
+	</LayoutTile>
+</template>
+
+<script lang="ts" setup>
+	definePageMeta({
+		name: "OS Informations",
+		icon: "lucide:info"
+	});
+
+	const items = ref([
+		{
+			label: "System",
+			icon: "lucide:monitor",
+			content: `${useTauriOsPlatform()} ${useTauriOsVersion()}`
+		},
+		{
+			label: "Arch",
+			icon: "lucide:microchip",
+			content: useTauriOsArch()
+		},
+		{
+			label: "Locale",
+			icon: "lucide:globe",
+			content: await useTauriOsLocale() || "Not detectable"
+		}
+	]);
+</script>
